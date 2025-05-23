7fd98224701ead13e4ff832af33828d28216ae0f
Mohammed Musharaf <mk.guvi123@gmail.com>
2025-05-23 14:15:58 +0000

    [FIX] fixed alignments

--- app/blog/_components/Newsletter.tsx
+++ app/blog/_components/Newsletter.tsx
@@ -8,7 +8,7 @@ export default function Newsletter() {
       <p className="text-gray-600 dark:text-gray-300 mb-6">
         Subscribe to our newsletter for the latest articles and updates
       </p>
-      <form className="max-w-md  flex-wrap flex gap-4">
+      <form className="max-w-md  flex-wrap mx-auto flex gap-4">
         <Input
           type="email"
           placeholder="Enter your email"
